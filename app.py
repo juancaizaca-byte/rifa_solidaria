c186cd9391d9a5af05aac6c42e311be7b275f815
JuanCarlosCA27 <juancaizaca@gmail.com>
2026-05-25 10:05:28 -0500

    Cambio por password

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import io
 
 def conectar():
     return psycopg2.connect(
-        "postgresql://postgres:Jasdevito27.@db.dzenrnhwqjvwlbpbukxc.supabase.co:5432/postgres"
+        "postgresql://postgres:Jasdevito27%2E@db.dzenrnhwqjvwlbpbukxc.supabase.co:5432/postgres?sslmode=require"
     )
 
 # Título principal de la app
