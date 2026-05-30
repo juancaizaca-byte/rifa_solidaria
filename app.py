1a039a90a66cde9b52b1662a12ac291fa03628c8
JuanCarlosCA27 <juancaizaca@gmail.com>
2026-05-30 15:59:24 -0500

    git commit -m "Update port"

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ def conectar():
     user = os.getenv("DB_USER")
     password = os.getenv("DB_PASS")
     database = os.getenv("DB_NAME")
-
+    port = int(os.getenv("DB_PORT"))
+    
     return mysql.connector.connect(
         host=host,
         user=user,
